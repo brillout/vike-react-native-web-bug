953670a90f46a89511c004d5b9f13fa850a09091
Romuald Brillout <git@brillout.com>
2024-05-13 20:14:20 +0200

    failed attempt to workaround RN Vite SSR issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,18 @@ import ssr from "vike/plugin";
 import react from "@vitejs/plugin-react";
 import reactNativeWeb from "vite-plugin-react-native-web";
 import { defineConfig } from "vite";
+import { cjsInterop } from "vite-plugin-cjs-interop";
 
 export default defineConfig({
-	plugins: [react({}), ssr({}), reactNativeWeb()],
+	plugins: [react({}), ssr({}), reactNativeWeb(), cjsInterop({
+    dependencies: [
+      "inline-style-prefixer",
+      "inline-style-prefixer/**"
+    ]
+  })],
+  ssr: {
+    optimizeDeps: {
+      include: ['react-native-web']
+    }
+  }
 });
